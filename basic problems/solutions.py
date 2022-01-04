6864ed254b0fae712aa2542f7caa53ddbc11dc62
gurveershienh <ggshienh@gmail.com>
2022-01-04 16:21:42 -0500

    Committing solutions.py and its _test.py

--- basic problems/solutions.py
+++ basic problems/solutions.py
@@ -1,7 +1,4 @@
 
-import os
-
-print(os.listdir())
 '''
 Problem 1: DNA nucleotide counter
 
@@ -23,30 +20,11 @@ def nucleotide_counter(s):
     for n in s:
         if n in count:
             count[n] += 1
-        elif n not in ['A', 'G', 'C', 'T']:
+        elif n not in ["A", "G", "T", "C"]:
             return "Error: Unknown nucleotide in sequence."
         else:
             count[n] = 1
     return count
-
-with open('rosalind_dna.txt', 'r') as file:      
-    DNA1 = file.read().rstrip()
-with open('rosalind_dna (1).txt', 'r') as file:
-    DNA2 = file.read().rstrip()
-
-    
-print("\nProblem 1: Counting DNA nucleotides\n\nTesting using random downloaded DNA sequences from https://rosalind.info/problems/dna/:\n")
-
-print(nucleotide_counter(DNA1))                  ## Testing using random downloaded DNA sequences from https://rosalind.info/problems/dna/
-print(nucleotide_counter(DNA2))
-
-print("\nTesting edge cases (s == '', s == 'XXXXX', s == '1234'):\n")
-
-print(nucleotide_counter(""))                     ## Testing edge cases (s == '', s == 'XXXXX', s == '1234')
-print(nucleotide_counter("TTTTT"))
-print(nucleotide_counter("1234")+"\n")
-
-
 
 '''
 Problem 2: Transcribing DNA into RNA
--- basic problems/solutions_test.py
+++ basic problems/solutions_test.py
@@ -1,14 +1,17 @@
-import solutions
+from solutions import nucleotide_counter
 import os
 
-print(os.listdir())
 
-
-
+DNA1 = open("data/rosalind_dna.txt", "r").read()
+DNA2 = open("data/rosalind_dna (1).txt", "r").read()
+print(DNA1)
 def test_nucleotide_counter():
-    assert solutions.nucleotide_counter('') == {'T': 218, 'A': 221, 'G': 222, 'C': 244}
-    assert solutions.nucleotide_counter('DNA2') == {'C': 219, 'A': 232, 'T': 236, 'G': 236}
-    assert solutions.nucleotide_counter('') == {}
-    assert solutions.nucleotide_counter('AAAAA') == {'T': 5}
-    assert solutions.nucleotide_counter('1234') == "Error: Unknown nucleotide_counter in sequence."
+    ## Testing using random downloaded DNA sequences from https://rosalind.info/problems/dna/
+    assert nucleotide_counter(DNA1) == {'A': 221, 'C': 244, 'G': 222, 'T': 218} 
+    assert nucleotide_counter(DNA2) == {'C': 219, 'A': 232, 'T': 236, 'G': 236}
+    ## Testing edge cases (s == '', s == 'XXXXX', s == '1234')
+    assert nucleotide_counter('') == {}
+    assert nucleotide_counter('TTTTT') == {'T': 5}
+    assert nucleotide_counter("1234") == "Error: Unknown nucleotide in sequence."
+    
     
